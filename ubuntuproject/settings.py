8cb5192035fd5352c20838221a5d87edbad3b07d
xingkongdadao <xingkongdadao@gmail.com>
2017-08-09 19:30:21 +0700

    Update settings.py

--- ubuntuproject/settings.py
+++ ubuntuproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8trwzhdf&pbm)^tfj24j9j640f$b3(vo_2f8ca8ur!%4rh9yb&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
